c10c994044c4cde94d5e6fbc2c8bb1c2a85e4b95
Max Schmitz <58770338+sjmxschm@users.noreply.github.com>
2022-07-21 18:34:12 +0200

    create create_model folder

---
+++ create_model/__init__.py
@@ -0,0 +1,4 @@
+'''
+File can be empty and is only important for the imports in the other files. The __init__.py file specifies in Python 2
+(which is used in Abaqus/CAE) that a folder is a package
+'''
