ca0069f5cee2a869315c94e4794bc6b54f4ae6ce
sjmxschm <st169819@stud.uni-stuttgart.de>
2022-12-05 13:44:19 -0500

    move parameter sets into different file to make code cleaner

---
+++ create_model/analze_existing_simulations.py
@@ -0,0 +1,7 @@
+"""
+analyze_existing_simulations looks into the existing simulations and casts the simulation information
+into a dataframe for further analysis. The idea is to get a list of existing simulations with
+given parameters to double-check if the parameter combination is already existing when new simulations
+are running.
+
+"""
