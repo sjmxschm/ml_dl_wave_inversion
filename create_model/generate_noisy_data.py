1158c5b790c3951f5566e08f2f97a320476c5929
sjmxschm <st169819@stud.uni-stuttgart.de>
2022-12-05 13:35:12 -0500

    created new file which analyzes the existing simulations on the cluster

--- create_model/generate_noisy_data.py
+++ create_model/generate_noisy_data.py
@@ -15,4 +15,31 @@ import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-from utils import load_2dfft_processed_data, invert_2dfft, reapply_2dfft, add_noise, store_fft_data, non_maximum_suppression, extract_features, lin_func
+from utils import (
+    get_newest_file_name,
+    load_2dfft_processed_data,
+    invert_2dfft,
+    reapply_2dfft,
+    add_noise,
+    store_fft_data,
+    non_maximum_suppression,
+    extract_features,
+    lin_func
+)
+
+# -- Specify folder to work in/where data lays
+# TODO: adjust this for the cluster/local machine with try except
+data_path = pathlib.Path().resolve() / '2dfft_data_selected' / '200._1._2._1._70'
+
+# -- Specify file names for example file
+# TODO: use my own function to do this
+fn = '11-09_19-25-13_max_analysis_job_disp_2dfft_fg_0.0002.csv'
+# check out if the following works:
+fn, is_transformed = get_newest_file_name(
+        data_path,
+        job_name='max_analysis_job',
+        extension='.csv'
+)
+
+fg, kg, abs_fft_data, sim_info = load_2dfft_processed_data(fn, data_path)
+print('>> frequency-/wavenumber grid, 2D-FFT data, and simulation information file was loaded!')
